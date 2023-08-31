73749d5560f324a7575b0a36b8101575f60832f0
aheesun-task <heesun.task@gmail.com>
2023-08-31 00:06:27 -0700

    add examples d1 comparing lazy and eager initialization

--- src/main/java/com/hs/learnspringframework/examples/d1/LazyInitializationLauncherApplication.java
+++ src/main/java/com/hs/learnspringframework/examples/d1/LazyInitializationLauncherApplication.java
@@ -4,14 +4,32 @@ import com.hs.learnspringframework.examples.c1.BusinessCalculationService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Component
+class ClassA {
+
+}
+@Component
+@Lazy
+class ClassB {
+    private ClassA classA;
+
+    public ClassB(ClassA classA) {
+        System.out.println("---ClassB constructor");
+        this.classA = classA;
+    }
+
+    public void doSth() {
+        System.out.println("do sth");
+    }
+}
 
 @Configuration
-@ComponentScan("com.hs.learnspringframework.examples.c1")
+@ComponentScan
 public class LazyInitializationLauncherApplication {
 
     public static void main(String[] args) {
@@ -20,11 +38,12 @@ public class LazyInitializationLauncherApplication {
                         (LazyInitializationLauncherApplication.class);
 
         try(context) {
-            Arrays.stream(context.getBeanDefinitionNames())
-                    .forEach(System.out::println);
+//            Arrays.stream(context.getBeanDefinitionNames())
+//                    .forEach(System.out::println);
 
-            var max = context.getBean(BusinessCalculationService.class).findMax();
-            System.out.println(max);
+            System.out.println("Initialization of context is completed");
+
+            context.getBean(ClassB.class).doSth();
         }
     }
 }
