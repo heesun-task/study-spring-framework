75a9f26b8628050faa1f9654a04b52ae9de720d0
aheesun-task <heesun.task@gmail.com>
2023-09-01 18:25:02 -0700

    init 2nd project repo to explore springboot

---
+++ 02-learn-spring-boot/src/main/java/com/heesun/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,20 @@
+package com.heesun.springboot.learnspringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "Udemy"),
+                new Course(2, "Learn AWS", "Udemy"),
+                new Course(3, "Learn AWS", "Udemy")
+        );
+    }
+}
