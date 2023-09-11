73538a8c84b9de31d81270e48ae0ac79e3777d8a
heesun-task <heesun.task@gmail.com>
2023-09-10 23:56:51 -0700

    Refactor createNewUser funciton

--- 04-web-application/src/main/java/com/hs/springboot/firstwebapp/security/SpringSecurityConfiguration.java
+++ 04-web-application/src/main/java/com/hs/springboot/firstwebapp/security/SpringSecurityConfiguration.java
@@ -13,19 +13,25 @@ import java.util.function.Function;
 @Configuration
 public class SpringSecurityConfiguration {
 
-    Function<String, String> passwordEncoder
-            = input -> passwordEncoder().encode(input);
-
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
+
+        UserDetails userDetails1 = createNewUser("hs", "dummy");
+        UserDetails userDetails2 = createNewUser("ds", "dummy2");
+
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
+        Function<String, String> passwordEncoder
+                = input -> passwordEncoder().encode(input);
         UserDetails userDetails = User.builder()
                                     .passwordEncoder(passwordEncoder)
-                                    .username("hs")
-                                    .password("dummy")
+                                    .username(username)
+                                    .password(password)
                                     .roles("USER", "ADMIN")
                                     .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
+        return userDetails;
     }
 
     @Bean
